e46ec4d35a63f988276a326e8cb722108c2ea75b
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-16 22:05:01 +0700

    Mengubah navbar menjadi dark ketika button-toggler ditekan

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,15 +2,45 @@ window.onscroll = () => {
   scrollFunction();
 };
 
+// Jika halaman discroll
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    document.getElementById("navbar").classList.remove("bg-transparent");
-    document.getElementById("navbar").classList.add("bg-dark");
-  } else {
-    document.getElementById("navbar").classList.remove("bg-dark");
-    document.getElementById("navbar").classList.add("bg-transparent");
+  let navbarTogglerStatus = document
+    .querySelector(".navbar-toggler")
+    .classList.contains("collapsed");
+
+  console.log(navbarTogglerStatus);
+
+  if (navbarTogglerStatus) {
+    if (document.documentElement.scrollTop > 100) {
+      changeNavbarDark();
+    } else {
+      changeNavbarTransparent();
+    }
   }
+}
+
+// Jika tombol hamburger ditekan
+document.querySelector(".navbar-toggler").addEventListener("click", () => {
+  let navbarTogglerStatus = document
+    .querySelector(".navbar-toggler")
+    .classList.contains("collapsed");
+
+  if (navbarTogglerStatus) {
+    if (document.documentElement.scrollTop > 100) {
+    } else {
+      changeNavbarTransparent();
+    }
+  } else {
+    changeNavbarDark();
+  }
+});
+
+function changeNavbarDark() {
+  document.getElementById("navbar").classList.remove("bg-transparent");
+  document.getElementById("navbar").classList.add("bg-dark");
+}
+
+function changeNavbarTransparent() {
+  document.getElementById("navbar").classList.remove("bg-dark");
+  document.getElementById("navbar").classList.add("bg-transparent");
 }
