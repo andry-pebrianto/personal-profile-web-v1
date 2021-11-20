59b4c586570ba4ceef269912e4dcfbce3d85142c
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-21 02:07:37 +0700

    Memperbaiki issue performa

---
+++ assets/js/copyright.js
@@ -0,0 +1,10 @@
+document.getElementById(
+  "copyright"
+).innerHTML = `Copyright &copy; ${new Date().getFullYear()}
+  <a
+    class="text-light fw-bold text-decoration-none"
+    href="https://www.facebook.com/andry.pebrianto/"
+    target="_blank"
+    rel="noreferrer"
+    >Andry Pebrianto</a
+  >`;
