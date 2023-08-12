1af0b81a6b848708c5e57341f01bec56c697509e
Andry Pebrianto <andrypeb227@gmail.com>
2023-08-12 21:29:23 +0700

    change hybrid to fullstack

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -6,16 +6,16 @@ var typewriter = new Typewriter(app, {
 });
 
 typewriter
-  .typeString(`Backend Developer`)
+  .typeString(`Backend Web Developer`)
   .pauseFor(2000)
   .deleteAll()
-  .typeString(`Frontend Developer`)
+  .typeString(`Frontend Web Developer`)
   .pauseFor(2000)
   .deleteAll()
   .typeString(`I Can Create Awesome Stuff`)
   .pauseFor(2000)
   .deleteAll()
-  .typeString(`Let's Work Together`)
+  .typeString(`Let's Work Together!`)
   .pauseFor(2000)
   .deleteAll()
   .start();
